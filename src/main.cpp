090e1befe7608d788d6e4e89a349a8299884e3f4
M3S7t <darkside1983@web.de>
2024-02-09 18:58:55 +0100

    Weiter Anpassungen. Funktionen für Uno aufwärts gegeben.

--- src/main.cpp
+++ src/main.cpp
@@ -33,19 +33,23 @@ int inputBus[] = {2, 3, 4, 5, 6, 7, 8, 9};                                  // I
 const uint8_t AnzahlInputStufen = sizeof(inputBus) / sizeof(inputBus[0]);   // autoamtisiserte Ermittlung der Sensoreingänge 
 const uint8_t AnzahlSensorenStufeKlein  = 5;                                // Anzahl Sensoren Stufe 01
 const uint8_t AnzahlSensorenStufe       = 6;                                // Anzahl Sensoren Stufe 02 - 08
+const uint8_t relaisOutput = 10; 
 uint8_t diffSensoren = 0;                                                   // Variable zm halten der Anzahl der Sensorabweichung Stufe 1 vs. 2 - 8                                            
-const uint8_t relaisOutput = 10;                                        
-float temperaturSchwelle = 21;                                              // Temperaturschwelle zur Auslösung
+float temperaturSchwelle = 24;                                              // Temperaturschwelle zur Auslösung
 bool schwelleUeberschritten = false;        
-uint8_t angesprocheneStufe = 0;       
-uint8_t angesprochenerSensor = 0;
-float angesprocheneTemp = 0;
-bool runCheckTemperatur = true;
+uint8_t angesprocheneStufe = 0;                                             // Stufe welche auf Temp.-Schwelle angesprochen hat
+uint8_t angesprochenerSensor = 0;                                           // Sensor welche auf Temp.-Schwelle angesprochen hat
+float angesprocheneTemp = 0;                                                // Temperatur bei Auslösung
+bool runCheckTemperatur = true;                                             // Run Temperaturcheck bzw, stop bei Auslösung
 
-String matrixAdressen[AnzahlInputStufen][AnzahlSensorenStufe];              // Matrix für Sensoradressen
 float temperatureMatrix[AnzahlInputStufen][AnzahlSensorenStufe];            // Matrix für aktuelle Sensor-Temperaturen
 float lastTemperatureMatrix[AnzahlInputStufen][AnzahlSensorenStufe];        // Matrix zur Sensorkontrolle
-// int adressenKonvert[8];
+
+
+// Wird genutzt für Funktion "speicherSensorAdresse()" >> nur mit über 16K SRAM möglich
+String matrixAdressen[AnzahlInputStufen][AnzahlSensorenStufe];              // Matrix für Sensoradressen
+String angesprocheneAdresse;                                                // Speicher für Sensoradresse
+
 
 OneWire oneWireBus[AnzahlInputStufen];
 DallasTemperature sensor[AnzahlInputStufen];
@@ -81,14 +85,43 @@ void sucheSensorAdresse() {
         for (int k = 0; k < 8; k++) {
           if (sensorAdresse[k] < 16) Serial.print("0");
           Serial.print(sensorAdresse[k], HEX);
-          if (sensorAdresse[k] < 16) matrixAdressen[i][j] += String("0");
-          matrixAdressen[i][j] += String(sensorAdresse[k],HEX); 
         }
       Serial.println();
     }
   }
 }
 
+void speicherSensorAdresse() {
+  // Nur bei Hardware mit mehr als 16k SRAM nutzbar
+
+    // Matrix für die Adressen initalisieren
+  for (int i = 0; i < AnzahlInputStufen; i++) {
+    for (int j = 0; j < AnzahlSensorenStufe; j++) {
+      matrixAdressen[i][j] = "";
+    }
+  }
+  Serial.println();
+  Serial.println("...Speicher der Adressen der Temperatur-Sensoren...");
+  Serial.println();
+
+  // Durchlauf durch die einzelnen Stufen
+  for (uint8_t i = 0; i < AnzahlInputStufen; i++) {
+    // Überprüfung ob Stufe 1 klein abgefragt wird >> nur 5x Sensoren
+    if (i == 0) diffSensoren = AnzahlSensorenStufeKlein;
+    else diffSensoren = AnzahlSensorenStufe;
+
+    for (uint8_t j = 0; j < diffSensoren; j++) {
+      // Adresse für jeden Sensor abrufen
+      DeviceAddress sensorAdresse;
+      sensor[i].getAddress(sensorAdresse, j);
+
+        for (int k = 0; k < 8; k++) {
+          if (sensorAdresse[k] < 16) matrixAdressen[i][j] += String("0"); 
+          matrixAdressen[i][j] += String(sensorAdresse[k], HEX); 
+          }
+    }
+  }
+}
 
 void abfrageTemperaturen() {
 for (int i = 0; i < AnzahlInputStufen; i++) {
@@ -138,8 +171,9 @@ void serialPrint() {
     Serial.print(angesprocheneStufe);
     Serial.print("] hat Sensor [");
     Serial.print(angesprochenerSensor);
-    Serial.print("] mit der Adresse : ");
-    Serial.print(matrixAdressen[angesprocheneStufe - 1][angesprochenerSensor - 1]);
+    Serial.print("]");
+    // Serial.print(" mit der Adresse : ");
+    // Serial.print(angesprocheneAdresse);
     Serial.print(" bei ");
     Serial.print(angesprocheneTemp);
     Serial.print("°C angesprochen !!!");
@@ -158,10 +192,12 @@ void checkTemperatur() {
     for (uint8_t j = 0; j < diffSensoren; j++) {
       if ( temperatureMatrix[i][j] > temperaturSchwelle ) {
         schwelleUeberschritten = true;
-        if (runCheckTemperatur)
+        if (runCheckTemperatur) {
         angesprocheneStufe = i + 1;
         angesprochenerSensor = j + 1;
         angesprocheneTemp = temperatureMatrix[i][j];
+        angesprocheneAdresse = String(matrixAdressen[0][0]);
+        }
         runCheckTemperatur = false;                     // Abschalten der checkTemperatur-Funktion um den Auslösewerte zu behalten.
       }
     }
@@ -185,6 +221,8 @@ void checkAusloesungRelais() {
 /*
 void ueberprüfungSensoren() {
 
+  Bei der aktuellen Hardware nicht möglich, da mehr Arbeitsspeicher benötigt wird.
+
   - Anzahl Sensoren je Stufe = 5x / 9x
   - Adressen vergleichen >> jede Adresse !=
   - Temperaturen vergleichen >> in zweites Array (current / last) kopieren
@@ -198,7 +236,7 @@ void ueberprüfungSensoren() {
 //-------------------------------------------------------------------------------------------------
 // Ende Funktionen
 
-// setup wuird nur beim Programm-Start ausgeführt
+// setup wird nur beim Programm-Start ausgeführt
 void setup() {
   if (runCheckTemperatur) pinMode(relaisOutput, OUTPUT);
   Serial.begin(9600);
@@ -209,9 +247,12 @@ void setup() {
     sensor[i] = DallasTemperature(&oneWireBus[i]);
     sensor[i].begin();
   }
-  
+
   // Adressenbestimmung der Sensoren
   sucheSensorAdresse();
+
+  // Funktion "speicherSensorAdresse()" bur bei Hardware mit mehr als 16k SRAM aktivieren
+  // speicherSensorAdresse();
 }
 
 // Programm in Schleife
@@ -230,5 +271,6 @@ void loop() {
   // Temperaturen ausgeben
   serialPrint();
   
-  delay(3000); // Pause von x Sekunden (von 1 Sekunde nach oben angepasst werden)
+  delay(1000); // Pause von x Sekunden (kann von 1 Sekunde nach oben angepasst werden)
+
 }
